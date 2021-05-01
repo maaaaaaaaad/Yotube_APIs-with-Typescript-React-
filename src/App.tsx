22dde7eabfd871c632e801fe515e7c877dfe26f0
오동웅 <woong@MacPro.local>
2021-05-01 20:33:39 +0900

    React.ts template

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React, { Component } from "react";
-import "./App.css";
+import React from "react";
+import ".App.css";
 
-class App extends Component {
-  render() {
-    return <div></div>;
-  }
-}
+const App = () => {
+  return <div></div>;
+};
 
 export default App;
